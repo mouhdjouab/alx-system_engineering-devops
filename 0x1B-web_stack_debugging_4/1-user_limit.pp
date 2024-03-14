9edea9de98a48d755e9be1cfec7c89c8f293f8fc
mouhdjouab <mouh.bw@gmail.com>
2024-03-14 02:38:42 +0100

    d

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Enable holberton to login and open file
+
+# Increase hard file limit
+exec { 'increase-hard-file':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit
+exec { 'increase-soft-file-limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
